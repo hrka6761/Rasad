561cbdd7bd8d5a70e677f430792e9fea79296412
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-20 19:08:09 +0330

    Creating UserRepoImpl.kt

---
+++ app/src/main/java/ir/srp/rasad/data/repositories/UserRepoImpl.kt
@@ -0,0 +1,31 @@
+package ir.srp.rasad.data.repositories
+
+import ir.srp.rasad.domain.repositories.UserRepo
+import javax.inject.Inject
+
+class UserRepoImpl @Inject constructor() : UserRepo {
+
+    override fun requestOTP() {
+        TODO("Not yet implemented")
+    }
+
+    override fun loginUser() {
+        TODO("Not yet implemented")
+    }
+
+    override fun registerUser() {
+        TODO("Not yet implemented")
+    }
+
+    override fun editMobileNumber() {
+        TODO("Not yet implemented")
+    }
+
+    override fun editUsername() {
+        TODO("Not yet implemented")
+    }
+
+    override fun editEmail() {
+        TODO("Not yet implemented")
+    }
+}
