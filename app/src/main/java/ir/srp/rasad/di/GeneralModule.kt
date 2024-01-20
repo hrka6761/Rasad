355932d46263da5c5f7125260a51bd4be3e342f6
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-20 18:38:52 +0330

    Creating hilt modules

---
+++ app/src/main/java/ir/srp/rasad/di/GeneralModule.kt
@@ -0,0 +1,9 @@
+package ir.srp.rasad.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class GeneralModule
