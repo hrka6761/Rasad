cd5309989d8e4e8149eb77374166e006533e0db7
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-01-23 18:43:21 +0330

    Adding settings button and its action

--- app/src/main/java/ir/srp/rasad/presentation/home/HomeFragment.kt
+++ app/src/main/java/ir/srp/rasad/presentation/home/HomeFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import ir.srp.rasad.R
+import ir.srp.rasad.core.BaseFragment
 import ir.srp.rasad.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private val viewModel: HomeViewModel by viewModels()
@@ -23,5 +25,24 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialize()
+    }
+
+
+    private fun initialize() {
+        initSettingsButton()
+    }
+
+    private fun initSettingsButton() {
+        binding.settingsImg.setOnClickListener { onClickSettings() }
+    }
+
+    private fun onClickSettings() {
+        navController.navigate(R.id.settingsFragment)
     }
 }
