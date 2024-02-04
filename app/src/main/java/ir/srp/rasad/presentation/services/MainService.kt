63c4e093021e4c39cd4832cd8d270e4cf2eb94e5
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-02-04 17:27:04 +0330

    Adding MainService.kt and adding it to the manifest

---
+++ app/src/main/java/ir/srp/rasad/presentation/services/MainService.kt
@@ -0,0 +1,24 @@
+package ir.srp.rasad.presentation.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
