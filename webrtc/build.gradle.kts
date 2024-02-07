bd0e605e78ccee03ad8cb91ea69470ea7213700f
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-02-07 13:38:14 +0330

    Transferring WebRTC dependency from app module to webrtc module

--- webrtc/build.gradle.kts
+++ webrtc/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // WebRTC
+    val webrtc_version = "1.0.23995"
+    implementation("org.webrtc:google-webrtc:$webrtc_version")
 }
